eada39eb4310401663e59ced2660cc6e1211ecd0
Max Elinson <max.elinson@gmail.com>
2025-03-31 00:59:10 -0400

    markov chain class init

---
+++ MarkovChain.py
@@ -0,0 +1,10 @@
+
+
+class MarkovChain:
+    def transition_probability(self, state1, state2):
+        return None
+
+    def transition_probability_indices(self, i, j):
+        return None
+
+    
